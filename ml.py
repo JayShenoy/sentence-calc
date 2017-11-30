bc507d24b3143a1bf0eb1b717d508e97c2816b78
Jay Shenoy <jay@jayshenoy.com>
2017-11-30 02:34:07 -0800

    Modify regressor to train with all examples

--- ml.py
+++ ml.py
@@ -3,20 +3,29 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 import pickle
 
-data = pd.read_csv('data3k.csv', dtype=np.int32)
+data = pd.read_csv('datafull.csv')
 variables = list(data)
 variable_index = {variables[i]: i for i in range(len(variables))}
 inputs = ['OFFTYPSB', 'NEWRACE', 'NEWEDUC', 'MONSEX', 'AGE', 'NEWCIT', 'DISTRICT', 'CRIMHIST']
-output = ['TOTPRISN']
+output = 'TOTPRISN'
 
-arr_data = data.as_matrix()
+# Remove rows with NaN values
+for i in inputs:
+	data = data[pd.notnull(data[i])]
+data = data[pd.notnull(data[output])]
+
+arr_data = data.as_matrix().astype(int)
 X = arr_data[:, [variable_index[i] for i in inputs]]
-Y = arr_data[:, [variable_index[i] for i in output]]
+Y = arr_data[:, variable_index[output]]
 
 new_index = {inputs[i]: i for i in range(len(inputs))}
 
 model = RandomForestRegressor(n_estimators=30)
-model.fit(X, Y[:, 0])
+model.fit(X, Y)
 
 with open('model.pkl', 'wb') as pickle_file:
 	pickle.dump(model, pickle_file)
+
+print('Model stored in model.pkl')
+print('R2 Value:', model.score(X, Y))
+print('Feature Importances', model.feature_importances_)
